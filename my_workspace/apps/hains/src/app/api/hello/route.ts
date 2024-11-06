53d2eb06dd4ead61f29a56cad14cceb7e25a47ae
AndreasGZ <andreasguzu@hotmail.de>
2024-11-06 09:20:48 +0100

    Fix hello route

--- my_workspace/apps/hains/src/app/api/hello/route.ts
+++ my_workspace/apps/hains/src/app/api/hello/route.ts
@@ -1,41 +1,36 @@
-import { prismaHains, getMonatsplanung } from '@my-workspace/prisma_hains';
+import { prismaHains } from '@my-workspace/prisma_hains';
 
 const db = prismaHains();
 
 export async function GET(request: Request) {
-  // const bedarfsEintraege = await db.bedarfs_eintrags.findMany({
-  //   include: {
-  //     schichts: true
-  //   },
-  //   where: {
-  //     tag: {
-  //       gte: new Date('2024-05-01'),
-  //       lte: new Date('2024-07-01')
-  //     }
-  //   }
-  // });
-  // const einteilungen = await db.diensteinteilungs.findMany({
-  //   where: {
-  //     tag: {
-  //       gte: new Date('2024-05-01'),
-  //       lte: new Date('2024-07-01')
-  //     }
-  //   }
-  // });
-  // return new Response(
-  //   JSON.stringify({
-  //     bedarfsEintraege,
-  //     einteilungen
-  //   }),
-  //   {
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   }
-  // );
-  return new Response(JSON.stringify(getMonatsplanung(10)), {
-    headers: {
-      'Content-Type': 'application/json'
+  const bedarfsEintraege = await db.bedarfs_eintrags.findMany({
+    include: {
+      schichts: true
+    },
+    where: {
+      tag: {
+        gte: new Date('2024-05-01'),
+        lte: new Date('2024-07-01')
+      }
     }
   });
+  const einteilungen = await db.diensteinteilungs.findMany({
+    where: {
+      tag: {
+        gte: new Date('2024-05-01'),
+        lte: new Date('2024-07-01')
+      }
+    }
+  });
+  return new Response(
+    JSON.stringify({
+      bedarfsEintraege,
+      einteilungen
+    }),
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  );
 }
