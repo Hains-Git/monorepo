9af88c1d4274356a2a436f6ec3b542b53ccdab77
AndreasGZ <andreasguzu@hotmail.de>
2024-11-07 10:06:50 +0100

    remove Apt

--- my_workspace/entry.sh
+++ my_workspace/entry.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 echo "Starting entry.sh in Path: $(pwd)"
+# apt-get updage
+# apt-get install net-tools -y
 
 npm install --force
 npx prisma generate
 
 echo "Show all running node processes"
+# netstat -anvp tcp | awk 'NR<3 || /LISTEN/'
 
 # Development
 npx nx serve hains --verbose
