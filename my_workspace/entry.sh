fe77f9238fe3422686bee6d6ca7599f8c6e8158c
AndreasGZ <andreasguzu@hotmail.de>
2024-11-08 16:05:57 +0100

    Docker dev mit Running NX Daemon for API watch-mode

--- my_workspace/entry.sh
+++ my_workspace/entry.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 echo "Starting entry.sh in Path: $(pwd)"
-# apt-get updage
-# apt-get install net-tools -y
+apt-get update
+apt-get install -y lsof
+echo "Show open Ports"
+lsof -i -P -n
+export NX_DAEMON=true
 
-npm install --force
+npm install --force 
 npx prisma generate
 
-# rm /tmp/f53b52ad6d21cceb72df/fp79.sock
+rm /tmp/f53b52ad6d21cceb72df/fp184.sock
 
+# Development
 npx nx run-many --target=serve --projects=hains,api --parallel --verbose
+# npx nx serve hains --verbose &
+# npx nx serve api --watch --verbose
+
+echo "Show open Ports after nx run-many"
+lsof -i -P -n
 
 # Production
 # npx nx build hains --prod --verbose
