d0c5aec6bc58291ad0afe695d523a43487e47eb3
AndreasGZ <andreasguzu@hotmail.de>
2024-11-08 14:51:22 +0100

    run_many

--- my_workspace/entry.sh
+++ my_workspace/entry.sh
@@ -7,16 +7,9 @@ echo "Starting entry.sh in Path: $(pwd)"
 npm install --force
 npx prisma generate
 
-<<<<<<< HEAD
-echo "Show all running node processes.."
-ps aux | awk '/node/{print $2}'
+# rm /tmp/f53b52ad6d21cceb72df/fp79.sock
 
-# rm /tmp/f53b52ad6d21cceb72df/fp78.sock
-rm /tmp/f53b52ad6d21cceb72df/fp79.sock
-
-# npx nx run-many --target=serve --projects=hains,api --parallel --verbose
-npx nx daemon start
-npx nx serve api
+npx nx run-many --target=serve --projects=hains,api --parallel --verbose
 
 # Production
 # npx nx build hains --prod --verbose
