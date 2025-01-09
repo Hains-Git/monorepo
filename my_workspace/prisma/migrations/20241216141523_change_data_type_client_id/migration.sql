0a923363e209b0c7e15ed4a89b7b6f7a16c55aa9
Juri Diener <you@example.com>
2025-01-09 14:07:05 +0100

    delete old migrations

--- my_workspace/prisma/migrations/20241216141523_change_data_type_client_id/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "oauth_access_tokens_new" ALTER COLUMN "client_id" SET DATA TYPE TEXT;
-
--- AlterTable
-ALTER TABLE "oauth_refresh_tokens" ALTER COLUMN "client_id" SET DATA TYPE TEXT;
