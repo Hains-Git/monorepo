2e8eec7e8f533b8eab00a5f2cb7a2d10bb5db920
AndreasGZ <andreasguzu@hotmail.de>
2025-01-27 16:51:53 +0100

    Add Unique Contraint to AccountInfos

--- my_workspace/apps/api/src/app/app.controller.ts
+++ my_workspace/apps/api/src/app/app.controller.ts
@@ -13,6 +13,7 @@ export class AppController {
 
   @Get('apidata')
   getApiData(@Query() query: { user_id: string }) {
+    console.log('Hallo', query);
     return this.appService.getApiData(Number(query?.user_id));
   }
 
