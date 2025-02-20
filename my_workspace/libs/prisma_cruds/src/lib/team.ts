42004e9d8e391258f82e052f7bc98c4f988df4b6
AndreasGZ <andreasguzu@hotmail.de>
2025-02-20 17:23:26 +0100

    Team Umziehen

--- my_workspace/libs/prisma_cruds/src/lib/team.ts
+++ my_workspace/libs/prisma_cruds/src/lib/team.ts
@@ -13,6 +13,30 @@ export const teamsBaseIncludes = {
   }
 };
 
+// def add_funktionen
+//   funktionen = params.has_key?(:funktionen_ids) ? JSON.parse(params[:funktionen_ids]).uniq.select { |el| !el.blank?} : []
+//   TeamFunktion.where(team_id: @team.id).delete_all
+//   funktionen.each { |id|
+//     TeamFunktion.find_or_create_by(team_id: @team.id, funktion_id: id.to_i)
+//   }
+// end
+
+// def add_team_kw_krankpuffers
+//   team_kw_krankpuffers = params.has_key?(:team_kw_krankpuffers) ?  JSON.parse(params[:team_kw_krankpuffers]) : []
+//   team_id = @team.id
+//   kws = []
+//   team_kw_krankpuffers.each do |obj|
+//     kw = obj["kw"].to_i
+//     puffer = obj["puffer"].to_i
+//     if kw > 0 && kw < 54 && puffer >= 0
+//       kws << kw
+//       team_kw_krankpuffer = TeamKwKrankpuffer.find_or_create_by(team_id: team_id, kw: kw)
+//       team_kw_krankpuffer.update(puffer: puffer)
+//     end
+//   end
+//   TeamKwKrankpuffer.where(team_id: team_id).where.not(kw: kws).delete_all
+// end
+
 export async function getAllTeams() {
   return await prismaDb.teams.findMany();
 }
@@ -112,13 +136,20 @@ async function checkTeamInput(input: TeamInputType, id: number, kostenstelle_id:
   else if (input.name.toLowerCase() === 'kein team') msg.push('Name nicht zulässig.');
   else if (input.name.includes('_')) msg.push('Name darf kein "_" enthalten.');
   if (!input.color) msg.push('Farbe nicht zulässig.');
-  if (id < 0) msg.push('ID muss größer als 0 sein.');
+  if (id < 0) msg.push('ID ist nicht zulässig.');
   if (input.krank_puffer < 0) msg.push('Krankpuffer muss größer oder gleich 0 sein.');
   if (!kostenStelle) msg.push('Kostenstelle nicht gefunden.');
   return msg.join('\n');
 }
 
-// add_funktionen und add_team_kw_krankpuffers fehlen noch
+export async function addTeamKrankpuffer() {
+  console.log('addTeamKrankpuffer');
+}
+
+export async function addTeamFunktionen() {
+  console.log('addTeamFunktionen');
+}
+
 export async function createOrUpdateTeam(args: TeamInputType & { id: number; kostenstelle_id: number }) {
   const input = {
     name: args.name.trim(),
