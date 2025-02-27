b174d31ae6c4699b7532d814f99d9e4d369abcb0
AndreasGZ <andreasguzu@hotmail.de>
2025-02-27 14:10:04 +0100

    SafeQl not working

--- my_workspace/libs/prisma_cruds/src/lib/diensteinteilung.ts
+++ my_workspace/libs/prisma_cruds/src/lib/diensteinteilung.ts
@@ -1,4 +1,18 @@
-import { Prisma } from '@prisma/client';
+import {
+  account_infos,
+  arbeitszeittyps,
+  bedarfs_eintrags,
+  diensteinteilungs,
+  dienstplanbedarves,
+  dienstplans,
+  einteilungskontexts,
+  einteilungsstatuses,
+  mitarbeiters,
+  po_diensts,
+  Prisma,
+  schichts,
+  teams
+} from '@prisma/client';
 import { prismaDb } from '@my-workspace/prisma_hains';
 import { startOfMonth, endOfMonth, parseISO, formatDate } from 'date-fns';
 import { formatDateForDB, getDateStr, newDate } from '@my-workspace/utils';
@@ -175,110 +189,146 @@ export async function getPossibleDienstfrei(tage: Date[], mitarbeiterIds: number
   const firstDate = tage[0];
   const lastMonth = newDate(firstDate);
   lastMonth.setMonth(lastMonth.getMonth() - 1);
-  const where: Prisma.diensteinteilungsWhereInput = {
-    tag: {
-      gte: lastMonth
-    },
-    mitarbeiters: {
-      platzhalter: false
-    },
-    einteilungsstatuses: onlyCounts ? { counts: true } : { counts: true, public: true },
-    po_diensts: {
-      bedarfs_eintrags: {
-        some: {
-          tag: {
-            in: tage
-          },
-          OR: tage.map((tag) => ({
-            first_bedarf: {
-              tag
-            }
-          }))
-        }
-      }
-    }
-  };
-  console.log(mitarbeiterIds);
-  if (mitarbeiterIds.length) {
-    where.mitarbeiter_id = {
-      in: mitarbeiterIds
-    };
-  }
-  const getEinteilungen = async (skip: number[]) =>
-    await prismaDb.diensteinteilungs.findMany({
-      where: {
-        ...where,
-        id: {
-          notIn: skip
-        }
-      },
-      take: 5000,
-      include: {
-        po_diensts: {
-          include: {
-            teams: true,
-            bedarfs_eintrags: {
-              include: {
-                first_bedarf: true
-              },
-              where: {
-                tag: {
-                  in: tage
-                }
-              }
-            }
-          }
-        },
-        mitarbeiters: {
-          include: {
-            account_info: true
-          }
-        },
-        einteilungsstatuses: true,
-        einteilungskontext: true,
-        dienstplans: {
-          include: {
-            dienstplanbedarves: true
-          }
-        }
-      },
-      orderBy: {
-        tag: 'asc'
-      }
-    });
-  // return await prismaDb.$queryRaw<diensteinteilungs[]>`
-  //   SELECT DISTINCT
-  //   de.*,
-  //   FROM diensteinteilungs AS de
-  //   JOIN einteilungsstatuses AS es ON es.id = de.einteilungsstatus_id
-  //   JOIN mitarbeiters AS m ON m.id = de.mitarbeiter_id
-  //   JOIN bedarfs_eintrags AS be ON be.po_dienst_id = de.po_dienst_id
-  //   AND be.tag = de.tag
-  //   AND de.bereich_id IS NULL OR de.bereich_id = be.bereich_id
-  //   LEFT OUTER JOIN dienstplans AS dpl ON (
-  //     dpl.dienstplanbedarf_id = be.dienstplanbedarf_id OR dpl.dienstplanbedarf_id IS NULL
-  //   ) AND de.dienstplan_id = dpl.id
-  //   JOIN bedarfs_eintrags AS be1 ON be1.first_entry = bedarfs_eintrags.first_entry
-  //   JOIN schichts AS s1 ON s1.bedarfs_eintrag_id = be1.id
-  //   JOIN arbeitszeittyps AS a1 ON a1.id = s1.arbeitszeittyp_id
-  //   AND (
-  //     ${tage
-  //       .map((tag) => {
-  //         return `(
-  //         de.tag < ${tag}
-  //         AND be1.tag < ${tag}
-  //         AND (
-  //           be1.tag + interval '1 days'*be1.ausgleich_tage >= ${tag}
-  //           OR s1.anfang::DATE + interval '1 days'*be1.ausgleich_tage > ${tag}
-  //           OR (
-  //             s1.ende::DATE > ${tag}
-  //             AND a1.dienstzeit = FALSE
-  //             AND a1.arbeitszeit = FALSE
-  //           )
-  //         )
-  //       )`;
-  //       })
-  //       .join(' OR ')}
-  //   )
-  // `;
+  console.log(lastMonth);
+  // Funktioniert noch nicht!!!!
+  const query = Prisma.sql(
+    [
+      `SELECT DISTINCT 
+      de.id, de.tag
+      FROM diensteinteilungs AS de
+      JOIN einteilungsstatuses AS es ON es.id = de.einteilungsstatus_id
+      JOIN mitarbeiters AS m ON m.id = de.mitarbeiter_id
+      JOIN bedarfs_eintrags AS be ON be.po_dienst_id = de.po_dienst_id 
+      AND be.tag = de.tag
+      AND de.bereich_id IS NULL OR de.bereich_id = be.bereich_id
+      LEFT OUTER JOIN dienstplans AS dpl ON (
+        dpl.dienstplanbedarf_id = be.dienstplanbedarf_id OR dpl.dienstplanbedarf_id IS NULL
+      ) AND de.dienstplan_id = dpl.id
+      JOIN bedarfs_eintrags AS be1 ON be1.first_entry = be.first_entry
+      JOIN schichts AS s1 ON s1.bedarfs_eintrag_id = be1.id
+      JOIN arbeitszeittyps AS a1 ON a1.id = s1.arbeitszeittyp_id
+      WHERE m.platzhalter = FALSE
+      AND `,
+      '',
+      'AND de.tag >=',
+      'AND (',
+      ') ORDER BY de.tag ASC',
+      ''
+    ],
+    onlyCounts ? 'es.counts = true' : 'es.counts = true AND es.public = true',
+    mitarbeiterIds.length ? `AND de.mitarbeiter_id IN (${mitarbeiterIds.join(',')}` : '',
+    getDateStr(lastMonth),
+    tage
+      .map((tag) => {
+        const dateStr = getDateStr(tag);
+        return `(
+          de.tag < '${dateStr}'
+          AND be1.tag < '${dateStr}'
+          AND (
+            be1.tag + interval '1 days'*be1.ausgleich_tage >= '${dateStr}'
+            OR s1.anfang::DATE + interval '1 days'*be1.ausgleich_tage > '${dateStr}'
+            OR (
+              s1.ende::DATE > '${dateStr}'
+              AND a1.dienstzeit = FALSE
+              AND a1.arbeitszeit = FALSE
+            ) 
+          )
+        )`;
+      })
+      .join(' OR ')
+  );
+  const einteilungenTagIds = (
+    await prismaDb.$queryRaw<
+      {
+        id: number;
+        tag: Date;
+      }[]
+    >(query)
+  ).reduce((acc: Record<string, number[]>, e) => {
+    const dateStr = getDateStr(e.tag);
+    acc[dateStr] ||= [];
+    acc[dateStr].push(e.id);
+    return acc;
+  }, {});
+  console.log(einteilungenTagIds);
+
+  type Einteilungen = (diensteinteilungs & {
+    einteilungskontext: einteilungskontexts | null;
+    einteilungsstatuses: einteilungsstatuses | null;
+    dienstplans:
+      | ({
+          dienstplanbedarves: dienstplanbedarves | null;
+        } & dienstplans)
+      | null;
+    mitarbeiters: ({ account_info: account_infos | null } & mitarbeiters) | null;
+    po_diensts:
+      | ({
+          teams: teams | null;
+          bedarfs_eintrags: ({
+            schichts: ({ arbeitszeittyps: arbeitszeittyps | null } & schichts)[];
+            first_bedarf:
+              | ({
+                  schichts: ({ arbeitszeittyps: arbeitszeittyps | null } & schichts)[];
+                } & bedarfs_eintrags)
+              | null;
+          } & bedarfs_eintrags)[];
+        } & po_diensts)
+      | null;
+  })[];
+
+  const results: Einteilungen = [];
+  // for (const tag in einteilungenTagIds) {
+  //   const ids = einteilungenTagIds[tag];
+  //   const date = newDate(tag);
+  //   const einteilungen: Einteilungen = await prismaDb.diensteinteilungs.findMany({
+  //     where: {
+  //       id: {
+  //         in: ids
+  //       },
+  //       tag: date
+  //     },
+  //     include: {
+  //       einteilungskontext: true,
+  //       einteilungsstatuses: true,
+  //       dienstplans: {
+  //         include: {
+  //           dienstplanbedarves: true
+  //         }
+  //       },
+  //       mitarbeiters: {
+  //         include: {
+  //           account_info: true
+  //         }
+  //       },
+  //       po_diensts: {
+  //         include: {
+  //           teams: true,
+  //           bedarfs_eintrags: {
+  //             include: {
+  //               schichts: {
+  //                 include: {
+  //                   arbeitszeittyps: true
+  //                 }
+  //               },
+  //               first_bedarf: {
+  //                 include: {
+  //                   schichts: {
+  //                     include: {
+  //                       arbeitszeittyps: true
+  //                     }
+  //                   }
+  //                 }
+  //               }
+  //             },
+  //             where: {
+  //               tag: date
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   });
+  //   results.concat(einteilungen);
+  // }
+  return results;
 }
