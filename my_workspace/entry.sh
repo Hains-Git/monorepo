d061295f1e556962158b9f76452a67195d73c0a1
AndreasGZ <andreasguzu@hotmail.de>
2025-03-20 12:15:12 +0100

    Undo entry changes

--- my_workspace/entry.sh
+++ my_workspace/entry.sh
@@ -3,48 +3,25 @@
 echo "Starting entry.sh in Path: $(pwd)"
 apt-get update
 apt-get install -y lsof
+echo "Show open Ports"
+lsof -i -P -n -U
 # export NX_DAEMON=true
+ls -R /tmp/ | grep "fp*.sock" 
+rm /tmp/f53b52ad6d21cceb72df/fp452.sock
+
 npm install --force
 
 npm run prisma:check
 npm run migrate:prod
 
-connect(){
-  echo "Trying to connect"
-  # Development
-  npx nx run-many --target=serve --projects=hains,api --configuration=development --parallel --verbose -- --inspect
-  
-  # Production
-  # npx nx run-many --target=build --configuration=production --projects=hains,api --parallel --verbose
-  # npx nx run-many --target=serve --configuration=production --projects=hains,api --parallel --verbose
-}
+# Development
+npx nx run-many --target=serve --projects=hains,api --configuration=development --parallel --verbose -- --inspect
 
-check_eadrinuse(){
-  echo "Checking EADDRINUSE"
-  connected=$(connect)
-  if [[ $connected == *"EADDRINUSE"* ]]; then
-    # Get .sock from error message
-    sock=$(echo $connected | grep -oP '/tmp/*/fp.*sock')
-    # remove sock
-    rm $sock
-    echo "Retry"
-  else
-    echo "Connected"
-  fi
-}
 
-count=0
-max=3
-echo "Starting connection loop"
-tryconnect=$(check_eadrinuse)
-while [[ $tryconnect == *"Retry"* ]]; do
-  echo "Not connected"
-  sleep 5
-  tryconnect=$(check_eadrinuse)
-  count=$((count+1))
-  if [ $count -eq $max ]; then
-    echo "Max retries reached"
-    break
-  fi
-done
-echo "Connection count: $count of $max"
+echo "Show open Ports after nx run-many"
+lsof -i -P -n -U
+
+# Production
+# echo "Start build"
+# npx nx run-many --target=build --configuration=production --projects=hains,api --parallel --verbose
+# npx nx run-many --target=serve --configuration=production --projects=hains,api --parallel --verbose
