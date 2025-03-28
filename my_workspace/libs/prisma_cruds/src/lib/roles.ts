397e20d11ebcb13190ade62cf4fa163a473ade25
AndreasGZ <andreasguzu@hotmail.de>
2025-03-28 10:06:53 +0100

    Mapping anzeigen

--- my_workspace/libs/prisma_cruds/src/lib/roles.ts
+++ my_workspace/libs/prisma_cruds/src/lib/roles.ts
@@ -7,17 +7,15 @@ class RolesMap {
     prismaDb.gruppes.findMany().then((roles) => {
       roles.forEach((role) => {
         if (!role.name) return;
-
         let name = role.name.trim();
         name = name.replace('HAINS', '').trim();
         name = name.replace('Anästhesie HD', '').trim();
-
         this.rolesMap.set(name, role.name);
       });
+
+      console.log('---roles', this.rolesMap.entries());
     });
   }
 }
 
 export const map = new RolesMap().rolesMap;
-
-console.log('---roles', map.entries());
